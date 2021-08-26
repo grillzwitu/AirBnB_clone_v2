88bbc6f85b8907d81da9e9c3b56fb9ea4a8f7702
grillzwitu <grillzwitu@gmail.com>
2021-08-26 14:12:27 +0000

    removed trailing white space

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 Generates a .tgz archive from the contents
 of the web_static folder of the AirBnB Clone repo
 """
-from os.path import exists 
+from os.path import exists
 from fabric.api import put, run, env
 
 
